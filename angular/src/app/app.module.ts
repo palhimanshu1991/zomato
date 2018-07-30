8385d5288bc31098175cdbd0241486c109bddbe8
Nisit Kaswan <nisitkaswan@instaveritas.com>
2018-07-30 18:57:01 +0530

    app.module.ts

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
