0c4f9241354b2feeb5112c047a8899a00c5b75a9
Nisit Kaswan <nisitkaswan@instaveritas.com>
2018-07-31 16:25:44 +0530

    Merge conflicts resolved

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { RestaurantListComponent } from './restaurant/restaurant-list/restaurant-list.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/login' , pathMatch:'full'},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
+  {path: 'restaurants', canActivate:[AuthGuard], component: RestaurantListComponent},
 
   {path:'home',canActivate:[AuthGuard], component: HomeComponent}
 ];
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,29 +5,33 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
 import { AuthGuard } from './auth/auth.guard';
+import { FooterComponent } from './components/footer/footer.component';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { RestaurantListComponent } from './restaurant/restaurant-list/restaurant-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    RestaurantsComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    FooterComponent,
+    RestaurantListComponent
   ],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RestaurantModule
   ],
   providers: [
     UserService,
