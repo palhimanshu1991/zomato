1469d9a536c39064b3059c9c679d9590cc0eeea6
Nisit Kaswan <nisitkaswan@instaveritas.com>
2018-07-31 17:00:17 +0530

    Updated layout and change logout function

--- angular/src/app/components/navbar/navbar.component.ts
+++ angular/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.userService.logout();
+    console.log('User logged out');
+    this.router.navigateByUrl('/');
   }
 
 }
