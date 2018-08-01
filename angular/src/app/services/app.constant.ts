988648fd8d0fb83e794476a5b4bc5f483b22b80b
Nisit Kaswan <nisitkaswan@instaveritas.com>
2018-08-01 12:05:33 +0530

    apiUrl changed

--- angular/src/app/services/app.constant.ts
+++ angular/src/app/services/app.constant.ts
@@ -1,12 +1,2 @@
-import { HttpHeaders } from '@angular/common/http';
+export const apiUrl = 'http://instafoods.test/api';
 
-export const apiUrl = 'http://127.0.0.1:8000/api';
-
-export const userToken = localStorage.getItem('userToken');
-export const reqHeaders = new HttpHeaders({
-    'Authorization': 'Bearer ' + userToken,
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Authorization, Content-Type'
-});
