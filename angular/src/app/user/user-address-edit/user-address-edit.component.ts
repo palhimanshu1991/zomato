90c5a91f1063364babab78fd162451a6a82ec0c5
Nisit Kaswan <nisitkaswan@instaveritas.com>
2018-08-02 17:19:20 +0530

    update

--- angular/src/app/user/user-address-edit/user-address-edit.component.ts
+++ angular/src/app/user/user-address-edit/user-address-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '../../../../node_modules/@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AddressService } from '../../services/address.service';
-import { ActivatedRoute, Router, ParamMap } from '../../../../node_modules/@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-address-edit',
